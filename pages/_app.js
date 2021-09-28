1f63087aa45d2a534e4af960a96e1ff3675dceb0
MantaRey <50933762+MantaRey@users.noreply.github.com>
2021-09-27 21:32:39 -0700

    Fixed SSR MaterialUI Styles
    Added _document.js file in order to change how the website is loaded in order to accommodate for the MaterialUI styling.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  //Use Effect is fix the issue with the rendering of server side Material UI elements
+  //Originally, when you refresh the page, the Material UI styling will be reverted back to default
+  // useEffect(() => {
+  //   const jssStyles = document.querySelector('#jss-server-side');
+  //   if (jssStyles) {
+  //     jssStyles.parent.removeChild(jssStyles);
+  //   }
+  // }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/core/styles';
+
+/*
+  Document was overwritten in order to solve the issue with the rendering of server side Material UI elements
+  Originally, when you refresh the page, the Material UI styling will be reverted back to default
+
+  Error:
+  "next/document should not be imported outside of pages/_document.js. See https://nextjs.org/docs/messages/no-document-import-in-page."
+  This Error makes no sense... I am inside of pages/_document.js
+  */
+
+export default class MyDocument extends Document {
+  redner() {
+    return (
+      <Html lang="en">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () => {
+    return originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+  };
+  const initialProps = await Document.getInitialProps(ctx);
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
