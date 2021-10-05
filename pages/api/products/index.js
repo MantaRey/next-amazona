42f62beb838f9730540266f21bff621dd4c192b9
MantaRey <50933762+MantaRey@users.noreply.github.com>
2021-10-04 20:16:18 -0700

    Build Login API
    Use jsonwebtoken to sign token, Implement login api

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -5,7 +5,6 @@ import db from '../../../utils/db';
 const handler = nextConnect();
 
 handler.get(async (req, res) => {
-  console.log('Retrieving Product');
   await db.connect();
   const products = await Product.find({});
   await db.disconnect();
--- pages/login.js
+++ pages/login.js
@@ -6,16 +6,32 @@ import {
   TextField,
   Typography,
 } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/Layout';
 import useStyles from '../utils/styles';
 import NextLink from 'next/link';
+import axios from 'axios';
 
 const Login = () => {
   const classes = useStyles();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post('/api/users/login', {
+        email,
+        password,
+      });
+      alert('success login');
+    } catch (err) {
+      alert(err.response.data ? err.response.data.message : err.message);
+    }
+  };
   return (
     <Layout title="Login">
-      <form className={classes.form}>
+      <form onSubmit={submitHandler} className={classes.form}>
         <Typography componet="h1" variant="h1">
           Login
         </Typography>
@@ -27,6 +43,7 @@ const Login = () => {
               id="email"
               label="Email"
               inputProps={{ type: 'email' }}
+              onChange={(e) => setEmail(e.target.value)}
             ></TextField>
           </ListItem>
 
@@ -37,6 +54,7 @@ const Login = () => {
               id="password"
               label="Password"
               inputProps={{ type: 'password' }}
+              onChange={(e) => setPassword(e.target.value)}
             ></TextField>
           </ListItem>
 
